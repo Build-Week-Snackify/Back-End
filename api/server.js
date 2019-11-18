a9dfdfb7cbb58b63fcc3186580e7bbd286f63693
Rayven <rayven.burns@gmail.com>
2019-11-18 10:52:18 -0500

    added server

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware');
+const authRouter = require('../auth/auth-router');
+const snackRouter = require('../snacks/snacks-router');
+const subRouter = require('../subscriptions/sub-router');
+const requestRouter = require('../request/request-router');
+const purchaseRouter = require('../one/one-router');
+
+
+const server = express();
+
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+
+server.use('/auth', authRouter);
+server.use('/snacks', authenticate, snackRouter);
+server.use('/subs', authenticate, subRouter);
+server.use('/request', authenticate, requestRouter);
+server.use('/purchase', authenticate, purchaseRouter);
+
+
+
+module.exports = server;
