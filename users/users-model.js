be9e1783600ab69e416154bbd1f72796dd2d014a
Rayven <rayven.burns@gmail.com>
2019-11-18 11:13:40 -0500

    added model for users and validation for username and password

---
+++ users/users-model.js
@@ -0,0 +1,72 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+    find,
+    insert, 
+     add,
+    findBy,
+    findEmBy,
+    findEmployeeById,
+    remove,
+    updateEmployee
+};
+
+function find() {
+    return db
+    .select('*')
+    .from('employee');
+};
+
+function findBy(filter) {
+    return db('org')
+    .where(filter)
+};
+
+function findEmBy(filter) {
+    return db('employee')
+    .where(filter)
+};
+
+function insert(org) {
+    return db('org')
+    .insert(org, 'id')
+    .then(ids => {
+        const id = ids[0]
+        return db('org')
+        .where({ id })
+        .first()
+    })
+};
+
+function add(employee) {
+    return db('employee')
+    .insert(employee, 'id')
+    .then(ids => {
+        const id = ids[0]
+        return db('employee')
+        .where({ id })
+        .first()
+    })
+};
+
+function findEmployeeById(id) {
+    return db('employee')
+    .where({ id })
+    .first();
+};
+
+function remove(id) {
+    return findById(id)
+    .then(user => {
+        return db('users')
+        .where({ id })
+        .del()
+        .then(() => user)
+    })
+};
+
+function updateEmployee(change, id) {
+    return db('employee')
+    .where({ id })
+    .update(change)
+};
